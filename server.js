2bbac810435ff20a98b42a895b71a17a216a5527
Nathan Bland <nathan.bland@gmail.com>
2016-08-29 11:20:40 -0600

    adjusting server.js path to fix serving of static files. Adjusting port number to 3000

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ mongoose.connect('mongodb://10.7.84.87/ping')
 var app = express()
 var server = require('http').Server(app)
 
-app.use(express.static('../static'))
+app.use(express.static('static'))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: false
 }))
 
-server.listen(app.get('port') || 8080, app.get('ip') || '0.0.0.0', function () {
+server.listen(app.get('port') || 3000, app.get('ip') || '0.0.0.0', function () {
   console.log('ping has started..')
 })
