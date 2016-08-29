a37c1abc9c8dc8c2522440a74c618193167fd1d6
Dhar <dharn@usxxdharnm1.corp.emc.com>
2016-08-29 15:01:45 -0600

    adding answers route

---
+++ server/routes/answers.js
@@ -0,0 +1,5 @@
+/**
+ * Created by dharn on 8/29/16.
+ */
+
+var Answer = require('../../mongo-models/questions');
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,11 @@ module.exports = function (express) {
   var router = express.Router()
 
   router.use('/users', require('./users')(express))
+  router.use('/questions', require('./questions')(express))
+  router.use('/answers', require('./answers')(express))
+
   router.get('/', function (req, res, next) {
-    res.json({msg: 'welcome to the api'})
+    res.json({msg: 'Welcome to the api'})
   })
   return router
 }
