5ce88d2fcd0cdffacc9b4a11536765b115fc780e
narrah <narrah@usennarrahm1.corp.emc.com>
2016-08-29 14:23:24 -0600

    Renamed files

--- static/app/signup.js
+++ static/app/signup.js
@@ -1,11 +1,9 @@
 /**
  * Created by narrah on 8/29/16.
  */
-var userDetailsApp = angular.module('userdetails', []);
+var userDetailsApp = angular.module('emcping', []);
 
-userDetailsApp.controller('UserLoginController', function($scope, $http){
-
-    $scope.signup = {};
+userDetailsApp.controller('signupController', function($scope, $http){
 
     $scope.areStringsEqual = function(password, confirmpassword){
         if(password==confirmpassword){
@@ -22,7 +20,7 @@ userDetailsApp.controller('UserLoginController', function($scope, $http){
         $http.post('http://128.222.159.134:3000/api/users/signup', $scope.formData)
             .then(function(data){
                 console.log("Successfull signup");
-                $scope.message = "Signup succesful";
+                $scope.message = "Signup successful";
             },
         function(response){
             console.log("Signup failed");
