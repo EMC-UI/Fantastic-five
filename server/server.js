faa1a188525469f0705fd5265999a24ac1b50fb8
Nathan Bland <nathan.bland@gmail.com>
2016-08-29 11:15:00 -0600

    boilerplate server.js file and serving of static files

---
+++ server/server.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+
+mongoose.connect('mongodb://10.7.84.87/ping')
+
+var app = express()
+var server = require('http').Server(app)
+
+app.use(express.static('../static'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+
+server.listen(app.get('port') || 8080, app.get('ip') || '0.0.0.0', function () {
+  console.log('ping has started..')
+})
