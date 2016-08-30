17367f8d0432b9cb6d22bed3a4bfeb1fc670cdfd
shyams1 <sridhar.shyamala@emc.com>
2016-08-30 13:24:21 -0600

    token fix in header

--- static/controllers/askQuestionCtrl.js
+++ static/controllers/askQuestionCtrl.js
@@ -4,14 +4,14 @@
 emcPingApp.controller('askQuestionCtrl', function($scope, loginService, $rootScope, $http){
         $scope.submit = function(){
             $scope.newformData = {};
-            $scope.newformData.token = loginService.getToken();
+            //$scope.newformData.token = loginService.getToken();
             $scope.newformData.title = $scope.formData.title;
             $scope.newformData.content = $scope.formData.content;
             $scope.newformData.tags = $scope.formData.tags;
 
 
             console.log($scope.newformData);
-            $http.post('http://128.222.159.134:3000/api/questions', $scope.newformData)
+            $http.post('http://128.222.159.134:3000/api/questions', $scope.newformData, {header: {'token': loginService.getToken()}})
                 .then(function(response){
                         console.log("Question Posted Successfully");
                         $scope.message = "Question Posted Successfully";
